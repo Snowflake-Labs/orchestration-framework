e6f3b7a4d68f064a0c58c3790d62afb0f8906ecc
Tyler White <tyler.white@snowflake.com>
2024-10-10 11:23:47 -0400

    chore: removing F401 violations (unused imports)

--- CortexCube/chains/llm_chain.py
+++ CortexCube/chains/llm_chain.py
@@ -6,7 +6,6 @@ import logging
 import warnings
 from typing import Any, Dict, List, Optional, Sequence, Tuple, Union
 
-from langchain.callbacks.base import BaseCallbackManager
 from langchain.callbacks.manager import (
     AsyncCallbackManager,
     AsyncCallbackManagerForChainRun,
@@ -15,7 +14,6 @@ from langchain.callbacks.manager import (
     Callbacks,
 )
 from langchain.load.dump import dumpd
-from langchain.load.serializable import Serializable
 from langchain.prompts.prompt import PromptTemplate
 from langchain.pydantic_v1 import Extra, Field
 from langchain.schema import (
