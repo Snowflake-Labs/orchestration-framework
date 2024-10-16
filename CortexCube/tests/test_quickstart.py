58b8100a4580e9743ab67afac1f36b04e2bd35f7
Tyler White <tyler.white@snowflake.com>
2024-10-16 14:52:23 -0400

    test: analyst tests and CortexCube tests for market cap

--- CortexCube/tests/test_quickstart.py
+++ CortexCube/tests/test_quickstart.py
@@ -30,3 +30,54 @@ def test_search_tool(session, question, answer):
     response = asyncio.run(annual_reports(question))
 
     assert answer in response[0].get("CHUNK")
+
+
+@pytest.mark.parametrize(
+    "question, answer",
+    [
+        pytest.param(
+            "What is the market cap of Apple, Inc?",
+            "       MARKETCAP\n0  3019131060224",
+            id="apple_market_cap",
+        ),
+        pytest.param(
+            "What is the market cap of Tesla?",
+            "      MARKETCAP\n0  566019162112",
+            id="tesla_market_cap",
+        ),
+    ],
+)
+def test_analyst_tool(session, question, answer):
+    analyst_config = {
+        "semantic_model": "sp500_semantic_model.yaml",
+        "stage": "SEMANTICS",
+        "service_topic": "S&P500 company and stock metrics",
+        "data_description": "a table with stock and financial metrics about S&P500 companies ",
+        "snowpark_connection": session,
+    }
+    sp500 = CortexAnalystTool(**analyst_config)
+    response = asyncio.run(sp500(question))
+
+    assert response == answer
+
+
+def test_cube_agent(session):
+    search_config = {
+        "service_name": "SEC_SEARCH_SERVICE",
+        "service_topic": "Snowflake's business,product offerings,and performance",
+        "data_description": "Snowflake annual reports",
+        "retrieval_columns": ["CHUNK"],
+        "snowpark_connection": session,
+    }
+    analyst_config = {
+        "semantic_model": "sp500_semantic_model.yaml",
+        "stage": "SEMANTICS",
+        "service_topic": "S&P500 company and stock metrics",
+        "data_description": "a table with stock and financial metrics about S&P500 companies ",
+        "snowpark_connection": session,
+    }
+    annual_reports = CortexSearchTool(**search_config)
+    sp500 = CortexAnalystTool(**analyst_config)
+    agent = CortexCube(snowpark_session=session, tools=[annual_reports, sp500])
+    response = agent("What is the market cap of Apple?")
+    assert "$3,019,131,060,224" in response
