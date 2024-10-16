8ed893a33f58de372da5c040f97932c1553ada4b
alejandro <alejandro.herrera@snowflake.com>
2024-10-16 11:25:47 -0400

    app cleanup

--- cube_demo_app/cube_demo_app.py
+++ cube_demo_app/cube_demo_app.py
@@ -1,5 +1,4 @@
 from dotenv import load_dotenv
-import snowflake
 from snowflake.snowpark import Session
 
 import streamlit as st
@@ -15,10 +14,9 @@ import requests
 
 from CortexCube import CortexCube, CortexAnalystTool, CortexSearchTool, PythonTool
 import warnings
+
 warnings.filterwarnings('ignore')
-
 load_dotenv("../.env")
-
 st.set_page_config(page_title="Snowflake Cortex Cube")
 
 connection_parameters = {
@@ -140,44 +138,28 @@ def process_message(prompt_id: str) -> None:
     tool_info_container = st.empty()
 
     with redirect_stdout_to_streamlit(output_container) as redirector:
-        # Call the analyst method and capture the output
+        
         response = asyncio.run(
             st.session_state.analyst.acall(prompt))['output']
 
-        # Extract tool information from the output
         lines = redirector.get_output().split('\n')
         sources = []
         for line in lines:
             tool_name = extract_tool_name(line)
             if tool_name is not None:
                 sources.append(tool_name)
-                # Display tool information
-                # tool_info_container.write(f"Using tool: {tool_name}")
 
-    # Construct the final response with tool information
     cleaned_sources = [source for source in set(sources) if source != '']
     source_output = ', '.join(list(set(cleaned_sources)))
     final_response = f"""{response} \n\n<span style="float:right;color:gray">Source: {source_output}</span>"""
     st.session_state['prompt_history'][prompt_id]['response'] = final_response
     st.rerun()
 
-# def extract_tool_name(yield_string):
-#     # Check if the input string matches the pattern
-#     if 'tool selected' in yield_string:
-#         # Extract the selected tool name
-#         words = yield_string.split('tool selected')
-#         selected_tool_name = words[0]  # Assuming the tool name is the first word
-#         if selected_tool_name.lower() != "finish":
-#             return selected_tool_name.strip()
-
-
 def extract_tool_name(statement):
     start = statement.find('Running') + len('Running') + 1
     end = statement.find('tool')
     return statement[start:end].strip()
 
-
-# Add custom CSS to resize the logo
 st.markdown("""
     <style>
         div[data-testid="stHeader"] > img, div[data-testid="stSidebarCollapsedControl"] > img {
@@ -193,29 +175,13 @@ st.markdown("""
 """, unsafe_allow_html=True)
 
 st.logo("SIT_logo_white.png")
-# st.title("Cortex Multi Agent Analyst")
+
 st.markdown(
     "<h1>🧠 Snowflake Cortex<sup style='font-size:.8em;'>3</sup></h1>",
     unsafe_allow_html=True,
 )
 st.caption(
     "A Multi-Agent System with access to Cortex, Cortex Search, Cortex Analyst, and more.")
-
-# for id in st.session_state.prompt_history:
-
-#     current_prompt = st.session_state.prompt_history.get(id)
-
-#     with st.chat_message('user'):
-#         st.write(current_prompt.get('prompt'))
-#     with st.chat_message('assistant'):
-
-#         if current_prompt.get('response') == 'waiting':
-
-#             with st.status("Awaiting Response", expanded=True):
-
-#                 st.write_stream(process_message(prompt_id=id))
-#         else:
-# st.write(st.session_state['prompt_history'][id]['response'],unsafe_allow_html=True)
 
 for id in st.session_state.prompt_history:
     current_prompt = st.session_state.prompt_history.get(id)
