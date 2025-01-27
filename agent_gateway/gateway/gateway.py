d0c3dd553314351d2d2882f06c453b952e5de54c
alejandro <alejandro.herrera@snowflake.com>
2025-01-27 08:43:10 -0500

    resolving main diff

--- agent_gateway/gateway/gateway.py
+++ agent_gateway/gateway/gateway.py
@@ -44,10 +44,6 @@ from agent_gateway.tools.snowflake_tools import (
 )
 
 from trulens.apps.custom import instrument, TruCustomApp
-<<<<<<< HEAD
-=======
-from trulens.connectors.snowflake import SnowflakeConnector
->>>>>>> eb725e8 (adding tool instrumentation)
 from trulens.core import TruSession
 
 from typing import ClassVar
@@ -159,11 +155,8 @@ class Agent:
     fuse: ClassVar[Any]
     acall: ClassVar[Any]
     handle_exception: ClassVar[Any]
-<<<<<<< HEAD
     run_async: ClassVar[Any]
     search_tool: Optional[CortexSearchTool] = None
-=======
->>>>>>> eb725e8 (adding tool instrumentation)
 
     def __init__(
         self,
